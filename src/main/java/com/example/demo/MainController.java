246e5a42cbd1b4aef272226d515695001dd61d5d
Takahiro Oku <pigment@mac.com>
2023-09-22 17:49:17 +0900

    first commit

---
+++ src/main/java/com/example/demo/MainController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    String hello(){
+        return ("hello");
+    }
+    @GetMapping("/support")
+    String support(){
+        return ("support");
+    }
+}
