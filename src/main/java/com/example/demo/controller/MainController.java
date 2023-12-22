81a7e110ffa2e7fc3f33e2c11e833d2c4cb3fcfd
ChaosReadman <pigment@mac.com>
2023-12-22 10:07:52 +0900

    Revert "adminuserページの通信と表示処理も、refresh.jsの関数を使うように変更"
    This reverts commit 724519820ff25951675792206f46143ac0916ebb, reversing
    changes made to e711af0f71bb5222db92b8d7deab06b1749b3536.

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -55,7 +55,6 @@ public class MainController {
         Account a = accounts.get(0);
         account.setUserName(a.getUserName());
         account.setNickName(a.getNickName());
-        account.setId(a.getId());
     }
 
     @GetMapping("/")
@@ -130,9 +129,8 @@ public class MainController {
     }
 
     @GetMapping("/adminuser")
-    public String adminUser(@AuthenticationPrincipal User user, @ModelAttribute Account account,BindingResult bind) {
+    public String adminUser(@AuthenticationPrincipal User user, @ModelAttribute Account account) {
         setAccountInfo(user, account);
-        
         return ("adminuser");
     }
 
@@ -143,64 +141,27 @@ public class MainController {
             @ModelAttribute(name = "modAccount") Account modAccount,
             @RequestParam(name = "id") int id) {
         setAccountInfo(user, account);
-
-        if (id == -1) {
-            modAccount.setId(-1);
-        } else {
-            Optional<Account> tmpAccount = accountr.findById(id);
-            modAccount.setUserName(tmpAccount.get().getUserName());
-            modAccount.setNickName(tmpAccount.get().getNickName());
-            modAccount.setPassword("");
-            modAccount.setAge(tmpAccount.get().getAge());
-            modAccount.setPrivileges(tmpAccount.get().getPrivileges());
-        }
-
+        Optional<Account> tmpAccount = accountr.findById(id);
+        System.out.println(tmpAccount.toString());
+        
+        modAccount.setUserName(tmpAccount.get().getUserName());
+        modAccount.setNickName(tmpAccount.get().getNickName());
+        modAccount.setPassword("");
+        modAccount.setAge(tmpAccount.get().getAge());
+        modAccount.setPrivileges(tmpAccount.get().getPrivileges());
         return ("modifyuser");
     }
 
     @PostMapping("updateuser")
     public String updateuser(@AuthenticationPrincipal User user, @ModelAttribute Account account,
-            @ModelAttribute(name = "modAccount") @Validated Account modAccount, BindingResult bind) {
+            @ModelAttribute(name = "modAccount") @Validated Account modAccount,BindingResult bind) {
         setAccountInfo(user, account);
-        if (bind.hasErrors()) {
+        if (bind.hasErrors()){
             return "modifyuser";
         }
-        modAccount.setLastUpdateUser(account.getUserName());
-        modAccount.setPassword(encoder.encode(modAccount.getOrgPassword()));
-        if (modAccount.getId() == -1) {
-            accountr.insert(modAccount);
-        } else {
-            accountr.update(modAccount);
-        }
-
-        modAccount.setOrgPassword("");
-        // もし自分と同じIDなら一度ログアウトが必要（なのでlogin画面に遷移）
-        if (modAccount.getId() != -1 && account.getId() == modAccount.getId()) {
-            return "login";
-        }
-        return "adminuser";
-    }
-
-    @GetMapping("/deleteuser")
-    public String deleteuser(
-            @AuthenticationPrincipal User user,
-            @ModelAttribute Account account,
-            BindingResult bind,
-            @RequestParam(name = "id") int id
-            ) {
-        setAccountInfo(user, account);
-        if(accountr.findAll().size() == 1){
-            // 最後の一人なので削除不可能
-            // 他にも、Privilegeとともに、findしないとだめかも
-            // まだPrivilegeの役割を決めていないので今はこのままにしておく
-            bind.rejectValue("id", "validation.deletion-of-account-impossible");
-            return "adminuser";
-        }
-        accountr.deleteById(id);
-        if(id==account.getId()){
-            return "login";
-        }
-        return "adminuser";
+        modAccount.setPassword(encoder.encode(modAccount.getPassword()));
+        System.out.println(modAccount.toString());
+        return "modifyuser";
     }
 
 }
--- src/main/java/com/example/demo/repository/AccountRepository.java
+++ src/main/java/com/example/demo/repository/AccountRepository.java
@@ -1,26 +1,11 @@
 package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.Account;
 
-import jakarta.transaction.Transactional;
-
 @Repository
-public interface AccountRepository extends JpaRepository<Account, Integer> {
-    Account findByUserName(String userName);
-
-    @Modifying
-    @Transactional
-    @Query(value="update account a set a.user_name=:#{#ac.userName}, a.nick_name=:#{#ac.nickName}, a.password=:#{#ac.password}, a.privileges=:#{#ac.privileges}, a.org_password=:#{#ac.orgPassword}, a.age=:#{#ac.age}, a.last_update_user=:#{#ac.lastUpdateUser}, a.last_update_date=CURRENT_TIMESTAMP where a.id=:#{#ac.id}", nativeQuery = true)
-    public abstract void update(@Param("ac") Account ac);
-
-    @Modifying
-    @Transactional
-    @Query(value="insert into account (user_name, nick_name, org_password, password, age, privileges, last_update_user ) values (:#{#ac.userName}, :#{#ac.nickName}, :#{#ac.orgPassword}, :#{#ac.password}, :#{#ac.age}, :#{#ac.privileges}, :#{#ac.lastUpdateUser})", nativeQuery = true)
-    public abstract void insert(@Param("ac") Account ac);
+public interface AccountRepository extends JpaRepository<Account,Integer>{
+    Account findByUserName(String userName); 
 }
