d6f70d6418214ba39141c1f42697947e6b8d1a49
namunamu-to <ten1baniraSS@gmail.com>
2024-04-03 17:16:32 +0900

    wysisygを使ってお知らせ投稿をできるようにした。ついでに見た目を整えた。

--- src/main/resources/static/js/wysiwygEditor.js
+++ src/main/resources/static/js/wysiwygEditor.js
@@ -1,81 +1,93 @@
 // 変数定義（最初に実行される）
 const editor = document.getElementsByClassName('simple-wysiwyg')[0];
-const toolbar = editor.getElementsByClassName('toolbar')[0];
-const buttons = toolbar.querySelectorAll('.editor-btn:not(.has-submenu)');
-const contentArea = editor.getElementsByClassName('content-area')[0];
-const visuellView = contentArea.getElementsByClassName('visuell-view')[0];
-const htmlView = contentArea.getElementsByClassName('html-view')[0];
-const modal = document.getElementsByClassName('modal')[0];
+const toolbar = document.getElementById('toolbar');
+const contentArea = document.getElementById('content-area');
+const modal = document.getElementById('modal');
+const visuallView = document.getElementById("visualView");
+const htmlView = document.getElementById("htmlView");
+const toolbarBtns = document.querySelectorAll(".editor-btn");
 
-// 選択領域の変更時イベントを定義
-document.addEventListener('selectionchange', selectionChange);
 
-// ペースト時のイベントを定義
-visuellView.addEventListener('paste', pasteEvent);
+//ツールバーの押されたボタンに応じた処理を実行
+for (let i = 0; i < toolbarBtns.length; i++) {
+  const btnId = toolbarBtns[i].getAttribute("id"); //判定用にボタン要素のidを取得する
+  toolbarBtns[i].addEventListener("click", (e) => {
+    if (btnId == "boldBtn") document.execCommand("bold"); //太字
+    else if (btnId == "italicBtn") document.execCommand("italic"); //斜体
+    else if (btnId == "underlineBtn") document.execCommand("underline"); //下線
+    else if (btnId == "strikethroughBtn") document.execCommand("strikethrough"); //取消線
+    else if (btnId == "alignCenterBtn") document.execCommand("justifyCenter"); //中央揃え
+    else if (btnId == "alignLeftBtn") document.execCommand("justifyLeft"); //左揃え
+    else if (btnId == "alignRightBtn") document.execCommand("justifyRight"); //右揃え
+    else if (btnId == "alignJustifyBtn") document.execCommand("justifyFull"); //両端揃え
+    else if (btnId == "numberedListBtn") document.execCommand("insertOrderedList"); //番号付きリスト
+    else if (btnId == "bulletedListBtn") document.execCommand("insertUnorderedList"); //リスト
+    else if (btnId == "outdentBtn") document.execCommand("outdent"); //インデントを減らす
+    else if (btnId == "indentBtn") document.execCommand("indent"); //インデントを増やす
+    else if (btnId == "horizontalLineBtn") document.execCommand("insertHorizontalRule"); //水平線
+    else if (btnId == "undoBtn") document.execCommand("undo"); //戻る
+    else if (btnId == "removeFormatBtn") document.execCommand("removeformat"); //文字修飾を削除。execCommandの仕様でブロック要素は残って無駄なタグが生まれるから注意。 
+    else if (btnId == "addLinkBtn") execLinkAction(); //リンク追加
+    else if (btnId == "deleteLinkBtn") document.execCommand("unlink"); //リンク削除
+    else if (btnId == "insertImgBtn") document.execCommand("insertImage"); //画像挿入
+    else if (btnId == "showHtmlBtn") htmlViewToggle();//HTMLコードの表示
+  }
+)}
 
-// キー押下時のイベントを定義
-contentArea.addEventListener('keypress', addParagraphTag);
-
-// ツールバーボタンクリックして時イベントを定義
-for (let button of buttons) {
-  button.addEventListener('click', function (e) {
-    let action = this.dataset.action;
-    if (action == 'toggle-view') execCodeAction(this, editor);
-    if (action == 'createLink') execLinkAction();
-    else execDefaultAction(action);
-  });
-}
-
-/**
- * ヴィシュアルビューとHTMLビューのトグルを行なう
- */
-function execCodeAction(button, editor) {
-  if (button.classList.contains('active')) { // show visuell view
-    visuellView.innerHTML = htmlView.value;
-    htmlView.style.display = 'none';
-    visuellView.style.display = 'block';
-
-    button.classList.remove('active');
-  } else {  // show html view
-    htmlView.innerText = visuellView.innerHTML;
-    visuellView.style.display = 'none';
-    htmlView.style.display = 'block';
-
-    button.classList.add('active');
+//htmlViewとvisualViewの表示・非表示を切り替える
+function htmlViewToggle() {
+  if (htmlView.style.display == "none") {
+    htmlView.innerHTML = visuallView.innerHTML;
+    htmlView.style.display = "block";
+    visuallView.style.display = "none"
+  } else {
+    visuallView.innerHTML = htmlView.value;
+    htmlView.style.display = "none";
+    visuallView.style.display = "block"
   }
 }
 
-/**
- * 現在選択エリアにリンクを設定する
- */
+// document.addEventListener('selectionchange', selectionChange); // 選択領域の変更時イベントを定義
+contentArea.addEventListener('keypress', addParagraphTag); // キー押下時のイベントを定義
+
+//リンク挿入ボタン押された時の処理
 function execLinkAction() {
+  //範囲選択されてないならreturn
+  if (document.getSelection().isCollapsed) return;
+
   modal.style.display = 'block';
+  //選択範囲を保存。なぜならユーザーがリンクを入力する際、visualViewの範囲選択が解除されるから。
   let selection = saveSelection();
 
   let submit = modal.querySelectorAll('button.done')[0];
-  let close = modal.querySelectorAll('.close')[0];
+  let close = document.getElementById('close');
 
   // done button active => add link
   submit.addEventListener('click', function (e) {
     e.preventDefault();
+    //要素取得
     let newTabCheckbox = modal.querySelectorAll('#new-tab')[0];
     let linkInput = modal.querySelectorAll('#linkValue')[0];
+
+    //ユーザーの入力を変数に保存
     let linkValue = linkInput.value;
     let newTab = newTabCheckbox.checked;
 
+    //選択範囲を復元
     restoreSelection(selection);
 
-    if (window.getSelection().toString()) {
-      let a = document.createElement('a');
-      a.href = linkValue;
-      if (newTab) a.target = '_blank';
-      window.getSelection().getRangeAt(0).surroundContents(a);
-    }
+    //選択範囲を<a href="" target="_blank"></a>で囲む
+    let a = document.createElement('a');
+    a.href = linkValue;
+    if (newTab) a.target = '_blank';
+    window.getSelection().getRangeAt(0).surroundContents(a);
 
+    //モーダルを非表示にする
     modal.style.display = 'none';
+    //テキストボックス内容を空白にする
     linkInput.value = '';
 
-    // deregister modal events
+    // ボタンのイベントリスナーを解除
     submit.removeEventListener('click', arguments.callee);
     close.removeEventListener('click', arguments.callee);
   });
@@ -94,71 +106,25 @@ function execLinkAction() {
   });
 }
 
-/**
- * この関数はノーマルイベントを処理する
- */
-function execDefaultAction(action) {
-  document.execCommand(action, false);
-}
-
-/**
- * 現在の選択範囲を保存する
- */
+//現在の選択範囲を保存する
 function saveSelection() {
-  if (window.getSelection) {
-    sel = window.getSelection();
-    if (sel.getRangeAt && sel.rangeCount) {
-      let ranges = [];
-      for (var i = 0, len = sel.rangeCount; i < len; ++i) ranges.push(sel.getRangeAt(i));
-
-      return ranges;
-    }
-  } else if (document.selection && document.selection.createRange) return document.selection.createRange();
-
-  return null;
-}
-
-/**
- *  保存した選択範囲をロードする
- */
-function restoreSelection(savedSel) {
-  if (savedSel) {
-    if (window.getSelection) {
-      sel = window.getSelection();
-      sel.removeAllRanges();
-      for (var i = 0, len = savedSel.length; i < len; ++i) sel.addRange(savedSel[i]);
-
-    } else if (document.selection && savedSel.select) savedSel.select();
-  }
-}
-
-/**
- * 現在の選択範囲をアクティブ・インアクティブに変更する
- */
-function selectionChange(e) {
-  for (let button of buttons) {
-
-    // don't remove active class on code toggle button
-    if (button.dataset.action === 'toggle-view') continue;
-
-    button.classList.remove('active');
+  selection = window.getSelection();
+  let ranges = [];
+  for (var i = 0, len = selection.rangeCount; i < len; ++i) {
+    ranges.push(selection.getRangeAt(i));
   }
 
-  if (!childOf(window.getSelection().anchorNode.parentNode, editor)) return false;
-
-  parentTagActive(window.getSelection().anchorNode.parentNode);
+  return ranges;
 }
 
-/**
- * 渡された子の親をチェックする
- */
-function childOf(child, parent) {
-  return parent.contains(child);
+//保存した選択範囲をロードする
+function restoreSelection(savedSelection) {
+  selection = window.getSelection();
+  selection.removeAllRanges(); //選択範囲に関する全てのrangeを削除
+  for (let i = 0; i < savedSelection.length; ++i) selection.addRange(savedSelection[i]);
 }
 
-/**
- * 現在の選択範囲にわたされたタグが設定可能かチェックする
- */
+//現在の選択範囲にわたされたタグが設定可能かチェックする
 function parentTagActive(elem) {
   if (!elem || !elem.classList || elem.classList.contains('visuell-view')) return false;
 
@@ -167,19 +133,21 @@ function parentTagActive(elem) {
   // active by tag names
   let tagName = elem.tagName.toLowerCase();
   toolbarButton = document.querySelectorAll(`.toolbar .editor-btn[data-tag-name="${tagName}"]`)[0];
-  if (toolbarButton) toolbarButton.classList.add('active');
+  if (toolbarButton) {
+    toolbarButton.classList.add('active');
+  }
 
   // active by text-align
   let textAlign = elem.style.textAlign;
   toolbarButton = document.querySelectorAll(`.toolbar .editor-btn[data-style="textAlign:${textAlign}"]`)[0];
-  if (toolbarButton) toolbarButton.classList.add('active');
+  if (toolbarButton) {
+    toolbarButton.classList.add('active');
+  }
 
   return parentTagActive(elem.parentNode);
 }
 
-/**
- * ペーストされたものをチェックしてHTMLを除去する
- */
+//ペーストされたものをチェックしてHTMLを除去する
 function pasteEvent(e) {
   e.preventDefault();
 
@@ -187,11 +155,9 @@ function pasteEvent(e) {
   document.execCommand('insertHTML', false, text);
 }
 
-/**
- * エンターキー押下時にpタグを追加する
- */
-function addParagraphTag(e) {
-  if (e.keyCode == '13') {
+//エンターキー押下時にパラグラフタグを追加する
+function addParagraphTag(evt) {
+  if (evt.keyCode == '13') {
 
     // don't add a p tag on list item
     if (window.getSelection().anchorNode.parentNode.tagName === 'LI') return;
